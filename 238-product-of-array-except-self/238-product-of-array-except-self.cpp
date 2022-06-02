d4d4e1b0f1bfd9bd6b7a1633a7437c80510ccb25
Sumit Kumar <49806908+skippersumit@users.noreply.github.com>
2022-06-02 22:30:05 +0530

    Time: 35 ms (48.20%), Space: 25.2 MB (15.57%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,23 +1,28 @@
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>prefixProductLR(n),prefixProductRL(n);
-        prefixProductLR[0] = nums[0];
-        for(int i=1;i<n;i++){
-            prefixProductLR[i] = prefixProductLR[i-1]*nums[i];
+class Solution
+{
+    public:
+        vector<int> productExceptSelf(vector<int> &nums)
+        {
+            int n = nums.size();
+            vector<int> prefixProductLR(n), prefixProductRL(n);
+            prefixProductLR[0] = nums[0];
+            for (int i = 1; i < n; i++)
+            {
+                prefixProductLR[i] = prefixProductLR[i - 1] *nums[i];
+            }
+            prefixProductRL[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                prefixProductRL[i] = prefixProductRL[i + 1] *nums[i];
+            }
+
+            vector<int> ans(n);
+            for (int i = 1; i < n - 1; i++)
+            {
+                ans[i] = prefixProductLR[i - 1] *prefixProductRL[i + 1];
+            }
+            ans[0] = prefixProductRL[1];
+            ans[n - 1] = prefixProductLR[n - 2];
+            return ans;
         }
-        prefixProductRL[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            prefixProductRL[i] = prefixProductRL[i+1]*nums[i];
-        }
-        
-        vector<int>ans(n);
-        for(int i=1;i<n-1;i++){
-            ans[i] = prefixProductLR[i-1]*prefixProductRL[i+1];
-        }
-        ans[0] = prefixProductRL[1];
-        ans[n-1] = prefixProductLR[n-2];
-        return ans;
-    }
 };

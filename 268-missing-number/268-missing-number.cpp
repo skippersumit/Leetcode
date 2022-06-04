ea2915f204a42a001d788cb28167c43124b9026e
Sumit Kumar <49806908+skippersumit@users.noreply.github.com>
2022-06-04 20:15:34 +0530

    Time: 32 ms (37.21%), Space: 18 MB (64.38%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,10 +1,12 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int sum = ((n+1)*(n))/2 ;
-        for(int i=0;i<n;i++)
-            sum -= nums[i];
-        return sum;
-    }
+class Solution
+{
+    public:
+        int missingNumber(vector<int> &nums)
+        {
+            int n = nums.size();
+            int sum = ((n + 1) *(n)) / 2;
+            for (int i = 0; i < n; i++)
+                sum -= nums[i];
+            return sum;
+        }
 };

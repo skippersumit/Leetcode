7f151fd017c29541c3ebd36007bf0dd34b690d43
Sumit Kumar <49806908+skippersumit@users.noreply.github.com>
2022-06-16 16:54:35 +0530

    Time: 26 ms (72.55%), Space: 14.2 MB (43.52%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+        bool iscycle(vector<int> adj[], vector<int> &visit, int node)
+        {
+            if (visit[node] == 1)
+            {
+                return true;
+            }
+
+            if (visit[node] == 0)
+            {
+                visit[node] = 1;
+                for (auto x: adj[node])
+                {
+                    if (iscycle(adj, visit, x))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            visit[node] = 2;
+            return false;
+        }
+
+    bool canFinish(int numc, vector<vector < int>> &pqt)
+    {
+       	// unordered_map<int,vector < int>>adj;
+       	//graph is ready 
+        vector<int> adj[numc];
+
+        for (auto x: pqt)
+        {
+            adj[x[1]].push_back(x[0]);
+        }
+
+       	//visit vector
+        vector<int> visit(numc, 0);
+
+        for (int i = 0; i < numc; i++)
+        {
+            if (iscycle(adj, visit, i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};

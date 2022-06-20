c1114bbd8bd5325fa22acdd970ce264c1db86967
Sumit Kumar <49806908+skippersumit@users.noreply.github.com>
2022-06-20 10:15:30 +0530

    Time: 108 ms (65.71%), Space: 11.4 MB (75.26%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -14,10 +14,14 @@ class Solution
             return 1e9;
         }
         if (dp[i][amount] != -1) return dp[i][amount];
+        
         int total = INT_MAX;
+        
         if (coins[i] <= amount)
             total = 1 + help(i, coins, amount - coins[i]);
+        
         total = min(total, help(i + 1, coins, amount));
+        
         return dp[i][amount] = total;
     }
     int coinChange(vector<int> &coins, int amount)

9af6818396714e355eb1a926eb60224144c1e30f
Sumit Kumar <49806908+skippersumit@users.noreply.github.com>
2022-07-24 22:02:36 +0530

    Time: 18 ms, Memory: 11 MB - LeetHub

---
+++ largest-number/largest-number.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+        static bool compare(int a, int b)
+        {
+            string s1 = to_string(a), s2 = to_string(b);
+            return s1 + s2 > s2 + s1;
+        }
+    string largestNumber(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end(), compare);
+        string ans = "";
+        for (auto num: nums)
+        {
+            ans += to_string(num);
+           	// cout<<num<<" ";
+        }
+        if (nums[0] == 0) return "0";
+
+        return ans;
+    }
+};

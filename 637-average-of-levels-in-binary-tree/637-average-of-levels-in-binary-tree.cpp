18b3f07d8340ae623a1018ee4f6456afcff96941
Sumit Kumar <49806908+skippersumit@users.noreply.github.com>
2022-09-02 12:49:36 +0530

    Time: 40 ms (7.38%), Space: 22.6 MB (32.47%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -1,33 +1,38 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector<double>ans;
-        if(root == NULL) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int sz = q.size();
-            double avg = 0.0;
-            for(int i=0;i<sz;i++){
-                TreeNode *topNode = q.front();q.pop();
-                avg += topNode->val;
-                if(topNode->left != NULL)q.push(topNode->left);
-                if(topNode->right != NULL)q.push(topNode->right);
+class Solution
+{
+    public:
+        vector<double> averageOfLevels(TreeNode *root)
+        {
+            vector<double> ans;
+            if (root == NULL) return ans;
+            queue<TreeNode*> q;
+            q.push(root);
+            while (!q.empty())
+            {
+                int sz = q.size();
+                double avg = 0.0;
+                for (int i = 0; i < sz; i++)
+                {
+                    TreeNode *topNode = q.front();
+                    q.pop();
+                    avg += topNode->val;
+                    if (topNode->left != NULL) q.push(topNode->left);
+                    if (topNode->right != NULL) q.push(topNode->right);
+                }
+                avg /= sz;
+                ans.push_back(avg);
             }
-            avg /= sz;
-            ans.push_back(avg);
+            return ans;
         }
-        return ans;
-    }
 };
